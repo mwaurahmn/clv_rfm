0197123e0b8ab8e6f652a82d7441cdd00590614f
mwaurahmn <100678788+mwaurahmn@users.noreply.github.com>
2023-09-21 18:56:57 +0300

    Product Bundling
    FP Growth Implementation

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,7 +1,14 @@
+# Data loading and preprocessing
 import pandas as pd
+
+# Visualization
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Visualization Display
 plt.rcParams['font.size'] = 20
 plt.rcParams['figure.figsize'] = (15, 8)
+
+# Modeling - Association Rule Mining
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import fpgrowth
